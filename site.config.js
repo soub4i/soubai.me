689fa6bd8de3de1bab78a9f72846d599585ecdfe
Abderrhim Soubai <gx.coder@gmail.com>
2020-11-03 02:05:01 +0100

    fix(core): description

--- site.config.js
+++ site.config.js
@@ -3,7 +3,7 @@ export default {
     title: "Soubai's stories",
     author: {
       name: "Abderrahim SOUBAI-ELIDRISI",
-      summary: " full stack JavaScript developer. I’m very interested in web Technologies & cloud computing & problem solving.",
+      summary: " full stack JavaScript developer. Interested in web Technologies & cloud computing & problem solving.",
     },
     description: "full stack JavaScript developer. I’m very interested in web Technologies & cloud computing & problem solving.",
     about: `My main passion as a web developer is to create memorable projects that deal with real-world problems and meet the needs of the business.`,
